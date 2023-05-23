59b3fe6a02d364081eba01ee9f7b591c15d0934d
Ajayakumar Managaon <conajma@partner.waters.com>
2023-05-23 08:32:28 +0530

    23052023:8:32AM

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from Employee import AttendanceDetails, Employee
+from EmployeeProfileDAL import EmployeeProfileDAL
+
 from flask import Flask,jsonify,json,redirect,url_for
 from flask import request , send_file , after_this_request
 from flask import render_template
@@ -13,6 +16,8 @@ from flask import session,g
 from logging.handlers import RotatingFileHandler
 
 
+
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
@@ -21,7 +26,92 @@ def home():
    return render_template('loginv4.html')
 
 
+#Employee Details API's - Create, Update and Delete Employee
 
+@app.route('/profile', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        sb = EmployeeProfileDAL()
+        rowReturn = sb.read_employee()
+        projectList=get_project_list()
+        return render_template("Dashboard.html", rowTable=rowReturn, projectList=projectList)
+    if request.method == 'POST':
+        corpid=request.form['corpId']
+        corppass = request.form['corppass']
+        sb = EmployeeProfileDAL()
+        EmployeeName = (sb.get_current_employee_Info(corpid))
+        rowReturn = sb.read_employee()
+        loginfailedmsg = "Invalid credentials"
+        projectList=get_project_list()
+        if corpid:
+            if request.method == 'POST':
+                session.pop('user', None)
+                if EmployeeName:
+                    session['user'] = request.form['corpId']
+                    app.logger.info('-------------------------------------------------------------------------------------')
+                    app.logger.info('Logged in by: %s', corpid)
+                    admin_return= Admin()
+                    if admin_return=="Yes":
+                        return redirect(url_for("viewTeamfun"))
+                    else:
+                        return render_template("Dashboard.html", rowTable=rowReturn, projectList=projectList)
+        app.logger.error('Failed to login for %s',corpid)
+        return render_template("loginV4.html", **locals())
+
+@app.route('/viewteam')
+def viewTeamfun():
+    if 'user' in session:
+        corp_id=session['user']
+        # corp_id="conddas"
+        sb=EmployeeProfileDAL()
+        EmployeeName=sb.get_current_employee_Info(corp_id)[0][0]
+        # projectName=sb.get_current_employee_Info()[0][]
+        print("In view team")
+        AdminReturn = Admin()
+        if AdminReturn == "Yes":
+            return render_template("viewteam.html", **locals())
+        else:
+            return render_template("LeaveAppPart2.html", EmployeeName=EmployeeName,
+                                   corpid=corp_id)
+    return render_template('loginV4.html', **locals())
+
+
+
+
+
+
+
+
+#All Private Methods
+def get_project_list():
+    projectList = []
+    for value in ReadJson()['project details']:
+        projectList.append(value['projectName'])
+    return projectList
+
+
+def ReadJson():
+    with open("static/json/pi.json",'r', encoding='utf-8-sig') as json_file:
+        json_data = json.load(json_file)
+    return json_data
+
+
+def Admin():
+    managers_corpid = []
+    for cid in ReadJson()['ManagersList']:
+        managers_corpid.append(cid['CorpID'])
+    print(managers_corpid)
+    for value in managers_corpid:
+        if session['user'] == value:
+            pass
+            return "Yes"
+    return "No"
+
+def get_employeeLevel_list():
+    employeeLevelList = []
+    for value in ReadJson()['EmployeeLevelDetails']:
+        employeeLevelList.append(value['levelName'])
+    return employeeLevelList
 
 
 
